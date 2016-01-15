384b3a0816fca50f193c50bd711929147a141f61
kielhong <mhhong@woowahan.com>
2016-01-15 18:08:15 +0900

    TDDBE xUnit

---
+++ WasRun.py
@@ -0,0 +1,21 @@
+from TestCase import TestCase
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        TestCase.__init__(self, name)
+
+    def setUp(self):
+        self.log = "setUp "
+
+    def testMethod(self):
+        self.log += "testMethod "
+
+    def testBrokenMethod(self):
+        raise Exception
+
+    def tearDown(self):
+        self.log += "tearDown "
+
+
+
